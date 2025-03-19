8e32d49a6bcf776080c759f0d873629117166971
Ali Arslan <arsalanli873@gmail.com>
2025-03-19 18:37:27 +0500

    Container and center widget

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("HOME",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54)
+            ),
+              backgroundColor: Colors.green,
+              elevation: 80,
+              centerTitle: true,
+            ),
+            body: Center(
+              child: Container(
+                  width: 300,
+                  height: 300,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      border: Border.all(color: Colors.black, width: 5),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Text(
+                    "Welcome to PC Parts Picker",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                  )),
+            )));
+  }
+}
